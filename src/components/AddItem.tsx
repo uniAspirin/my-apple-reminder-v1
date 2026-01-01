9f1c4b9f4f8f2db28d2962ea857a8c04388ead32
BOYONG CHEN <brianchen.me@outlook.com>
2026-01-01 04:56:12 +0000

    Item now can show all contents

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -18,11 +18,12 @@ export default function AddItem({ listId }: { listId: string }) {
         });
 
       setContent("");
+      e.currentTarget.style.height = "29px";
     }
   };
   return (
-    <div className="flex items-center gap-2 w-full">
-      <button>
+    <div className="flex items-start gap-2 w-full">
+      <button className="mt-0.5">
         <Circle className="text-neutral-300" />
       </button>
       {/* <input
@@ -33,11 +34,18 @@ export default function AddItem({ listId }: { listId: string }) {
         onChange={(e) => setContent(e.target.value)}
       /> */}
       <textarea
-        className="w-full bg-transparent border-b border-dashed border-neutral-300 text-neutral-800 outline-none px-1 py-0.5 resize-none overflow-hidden placeholder:text-neutral-300 h-7.25"
+        className="w-full bg-transparent border-b border-dashed border-neutral-300 text-neutral-800 outline-none px-1 py-0.5 resize-none overflow-hidden placeholder:text-neutral-300 h-[29px] min-h-[29px] leading-[20px]"
         placeholder="new todo"
         onKeyDown={handleKeyDown}
         value={content}
         onChange={(e) => setContent(e.target.value)}
+        onInput={(e) => {
+          const target = e.target as HTMLTextAreaElement;
+          target.style.height = "29px"; // 1. 重置回初始高度
+          // 2. 取 scrollHeight 和 29px 之间的最大值
+          const newHeight = Math.max(target.scrollHeight, 29);
+          target.style.height = `${newHeight}px`;
+        }}
       />
     </div>
   );
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -3,11 +3,26 @@ import type { TodoItem } from "../types/todo";
 import { useTodoStore } from "../hooks/useTodoStore";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { useEffect, useRef } from "react";
 
 export default function TodoItem({ todoItem }: { todoItem: TodoItem }) {
   const { id, content, isFinished, listId, position } = todoItem;
   const toggleIsFinished = useTodoStore((state) => state.toggleIsFinished);
   const editItemContent = useTodoStore((state) => state.editItemContent);
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const adjustHeight = () => {
+    const target = textareaRef.current;
+    if (target) {
+      target.style.height = "29px";
+      const newHeight = Math.max(target.scrollHeight, 29);
+      target.style.height = `${newHeight}px`;
+    }
+  };
+
+  useEffect(() => {
+    adjustHeight();
+  }, [content]);
 
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id, data: { role: "item", listId, position } });
@@ -19,13 +34,13 @@ export default function TodoItem({ todoItem }: { todoItem: TodoItem }) {
 
   return (
     <div
-      className="flex items-center gap-2 w-full"
+      className="flex items-start gap-2 w-full"
       ref={setNodeRef}
       style={style}
       {...attributes}
       {...listeners}
     >
-      <button onClick={() => toggleIsFinished(id)}>
+      <button onClick={() => toggleIsFinished(id)} className="mt-0.5">
         {isFinished ? (
           <div className="size-6 rounded-full overflow-clip bg-neutral-300 flex items-center justify-center">
             <Check size={13} strokeWidth={5} className="text-neutral-50" />
@@ -34,14 +49,15 @@ export default function TodoItem({ todoItem }: { todoItem: TodoItem }) {
           <Circle className="text-neutral-300" />
         )}
       </button>
-      <input
-        className={`border-b w-full border-neutral-300 outline-none px-1 py-0.5 ${
-          isFinished ? "text-neutral-600" : ""
-        }`}
+      <textarea
+        ref={textareaRef}
         value={content}
         onChange={(e) =>
-          editItemContent({ content: e.target.value, itemId: id })
+          editItemContent({ content: e.target.value.trim(), itemId: id })
         }
+        className={`border-b w-full border-neutral-300 outline-none px-1 py-0.5 resize-none ${
+          isFinished ? "text-neutral-600" : ""
+        }`}
       />
     </div>
   );
